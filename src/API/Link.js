6de029b0b686ac8adad3f67221ca980059423599
duongdiep31 <zenkyxz@gmail.com>
2023-04-01 15:44:33 +0700

    update

--- src/API/Link.js
+++ src/API/Link.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getLocal } from '../ultis/storage';
 const axiosClient = axios.create({
-	baseURL: 'http://localhost:8000/api',
+	baseURL: process.env.REACT_APP_API,
 });
 
 axiosClient.interceptors.request.use((req) => {
